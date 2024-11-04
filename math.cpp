4ea69d4cee0481b1e08987e0334e26da07681302
Rebelaniket <kumarniket924@gmail.com>
2024-11-04 14:20:49 +0530

    first commit

---
+++ math.cpp
@@ -0,0 +1,13 @@
+// program to add two numbers
+#include <iostream>
+using namespace std;
+int main() {
+    int num1, num2, sum;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    // sum of two numbers in stored in variable sum
+    sum = num1 + num2;
+    // Prints sum
+    cout << "Sum = " << sum;
+    return 0;
+}
